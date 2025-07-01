6b756e24f7bccfa41380bce0cd5b5d015ce401b7
Matthew Molloy <whamtet@gmail.com>
2025-07-02 01:26:47 +0900

    rationalize names

--- simpleui/assets/src/home.clj
+++ simpleui/assets/src/home.clj
@@ -1,7 +1,7 @@
-(ns <<ns-name>>.web.views.hello
+(ns <<ns-name>>.web.views.home
     (:require
       [simpleui.core :as simpleui :refer [defcomponent]]
-      [<<ns-name>>.web.htmx :refer [page-htmx]]))
+      [<<ns-name>>.web.page :refer [page-htmx]]))
 
 (defcomponent ^:endpoint hello [req my-name]
   [:div#hello "Hello " my-name])
--- simpleui/assets/src/ui.clj
+++ simpleui/assets/src/ui.clj
@@ -2,7 +2,7 @@
   (:require
    [<<ns-name>>.web.middleware.exception :as exception]
    [<<ns-name>>.web.middleware.formats :as formats]
-   [<<ns-name>>.web.views.hello :as hello]
+   [<<ns-name>>.web.views.home :as home]
    [integrant.core :as ig]
    [reitit.ring.middleware.muuntaja :as muuntaja]
    [reitit.ring.middleware.parameters :as parameters]))
@@ -26,4 +26,4 @@
   [_ {:keys [base-path]
       :or   {base-path ""}
       :as   opts}]
-  [base-path (route-data opts) (hello/ui-routes base-path)])
+  [base-path (route-data opts) (home/ui-routes base-path)])
